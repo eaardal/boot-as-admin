ee1f176102b268aea0618432b634bcad0b5a30ee
Eirik Årdal <eiard@statoil.com>
2016-02-15 15:09:18 +0100

    Cleanup

--- BootAsAdmin/Program.cs
+++ BootAsAdmin/Program.cs
@@ -19,32 +19,21 @@ namespace BootAsAdmin
                 return;
             }
 
-            foreach (var kvp in bootlist)
+            foreach (var entry in bootlist)
             {
                 try
                 {
-                    var applicationPath = kvp.Item1;
-                    var applicationArgs = kvp.Item2;
-                    var workingDir = kvp.Item3;
+                    var applicationPath = entry.Item1;
+                    var applicationArguments = entry.Item2;
+                    var workingDirectory = entry.Item3;
 
-                    var startInfo = new ProcessStartInfo(applicationPath)
-                    {
-                        Verb = "runas",
-                        UseShellExecute = true,
-                        Arguments = applicationArgs,
-                        WorkingDirectory = workingDir
-                    };
+                    Console.WriteLine($"Attempting to start {applicationPath} with args: {applicationArguments}, in working directory: {workingDirectory}");
 
-                    var process = Process.Start(startInfo);
-
-                    if (process != null)
-                    {
-                        Console.WriteLine($"Process {process.Id} started for {applicationPath}");
-                    }
+                    StartProcess(applicationPath, applicationArguments, workingDirectory);
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine($"Exception: {ex.Message}\n{ex.StackTrace}");
+                    WriteException(ex);
                 }
             }
 
@@ -54,45 +43,77 @@ namespace BootAsAdmin
             }
         }
 
-        private static void Exit()
+        private static void StartProcess(string applicationPath, string applicationArguments, string workingDirectory)
         {
-            Console.WriteLine("Press any key to exit");
-            Console.ReadKey();
+            var startInfo = new ProcessStartInfo(applicationPath)
+            {
+                Verb = "runas",
+                UseShellExecute = true,
+                Arguments = applicationArguments,
+                WorkingDirectory = workingDirectory
+            };
+
+            var process = Process.Start(startInfo);
+
+            if (process != null)
+            {
+                Console.WriteLine($"Process {process.Id} started for {applicationPath}");
+            }
         }
 
         private static IEnumerable<Tuple<string, string, string>> ReadBootlist()
         {
-            var exePath = Assembly.GetExecutingAssembly().Location;
-
-            var exeDir = Path.GetDirectoryName(exePath);
-
-            if (exeDir == null)
+            try
             {
+                var assemblyLocation = Assembly.GetExecutingAssembly().Location;
+
+                var assemblyDirectory = Path.GetDirectoryName(assemblyLocation);
+
+                if (assemblyDirectory == null)
+                {
+                    return null;
+                }
+
+                var bootlistFilePath = Path.Combine(assemblyDirectory, "Bootlist.txt");
+
+                if (File.Exists(bootlistFilePath))
+                {
+                    return
+                        File.ReadAllLines(bootlistFilePath)
+                            .Where(line => !line.StartsWith("#"))
+                            .Select(line => line.Split(';'))
+                            .Select(ParseLine);
+                }
+
+                Console.WriteLine("Couldn't find Bootlist.txt. It should exist in same dir as BootAsAdmin.exe");
                 return null;
             }
-
-            var bootlistFilePath = Path.Combine(exeDir, "Bootlist.txt");
-
-            if (File.Exists(bootlistFilePath))
+            catch (Exception ex)
             {
-                return
-                    File.ReadAllLines(bootlistFilePath)
-                        .Where(line => !line.StartsWith("#"))
-                        .Select(line => line.Split(';'))
-                        .Select(ParseLine);
+                Console.WriteLine("Something went wrong when attempting to read Bootlist.txt. Ensure it exists and has the correct format: exe-path;exe-arguments;working-dir");
+                WriteException(ex);
+                return null;
             }
-
-            Console.WriteLine("Couldn't find Bootlist.txt. It should exist in same dir as BootStatoil.exe");
-            return null;
         }
 
         private static Tuple<string, string, string> ParseLine(string[] lineElements)
         {
             var exePath = lineElements[0];
-            var arguments = lineElements.Length >= 2 ? lineElements[1] : string.Empty;
-            var workingDir = lineElements.Length == 3 ? lineElements[2] : string.Empty;
+            var exeArguments = lineElements.Length >= 2 ? lineElements[1] : string.Empty;
+            var workingDirectory = lineElements.Length == 3 ? lineElements[2] : string.Empty;
 
-            return new Tuple<string, string, string>(exePath, arguments, workingDir);
+            return new Tuple<string, string, string>(exePath, exeArguments, workingDirectory);
+        }
+
+        private static void Exit()
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+        
+        private static void WriteException(Exception ex)
+        {
+            Console.WriteLine($"Exception: {ex.Message}\n{ex.StackTrace}");
         }
     }
 }
